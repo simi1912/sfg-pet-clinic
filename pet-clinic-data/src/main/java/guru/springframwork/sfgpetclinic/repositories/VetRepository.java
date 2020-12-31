9a9f7d542fa02545afd421da68d864856d48ab4d
simi1912 <nioasca@gmail.com>
2020-12-31 16:40:58 +0200

    Added spring data service repository for owner. close #40

--- pet-clinic-data/src/main/java/guru/springframwork/sfgpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframwork/sfgpetclinic/repositories/VetRepository.java
@@ -1,6 +1,7 @@
 package guru.springframwork.sfgpetclinic.repositories;
 
+import guru.springframwork.sfgpetclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface Vet extends CrudRepository<Vet, Long> {
+public interface VetRepository extends CrudRepository<Vet, Long> {
 }
--- pet-clinic-data/src/main/java/guru/springframwork/sfgpetclinic/repositories/Visit.java
+++
@@ -1,6 +0,0 @@
-package guru.springframwork.sfgpetclinic.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface Visit extends CrudRepository<Visit, Long> {
-}
