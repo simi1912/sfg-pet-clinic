8fec7bd73252dfe5c550091d374eb90b19c0520c
Ioan Simiciuc <nioasca@gmail.com>
2021-01-11 10:09:49 +0200

    adding model attributes for Pet controller. close #56

---
+++ pet-clinic-web/src/main/java/guru/springframwork/sfgpetclinic/controllers/PetController.java
@@ -0,0 +1,50 @@
+package guru.springframwork.sfgpetclinic.controllers;
+
+import guru.springframwork.sfgpetclinic.model.Owner;
+import guru.springframwork.sfgpetclinic.model.PetType;
+import guru.springframwork.sfgpetclinic.services.OwnerService;
+import guru.springframwork.sfgpetclinic.services.PetService;
+import guru.springframwork.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    public static final String VIEWS_PETS_CREATE_OR_UPDATE_FROM =
+            "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService,
+                         OwnerService ownerService,
+                         PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes(){
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId){
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+}
