805fa15b03630abf9a150045401aa7288333ad05
Ioan Simiciuc <nioasca@gmail.com>
2021-01-11 18:24:06 +0200

    Adding/editing visit added.  #57

---
+++ pet-clinic-web/src/main/java/guru/springframwork/sfgpetclinic/controllers/VisitController.java
@@ -0,0 +1,54 @@
+package guru.springframwork.sfgpetclinic.controllers;
+
+import guru.springframwork.sfgpetclinic.model.Pet;
+import guru.springframwork.sfgpetclinic.model.Visit;
+import guru.springframwork.sfgpetclinic.services.PetService;
+import guru.springframwork.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class VisitController {
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable Long petId, Model model){
+        Pet pet = petService.findById(petId);
+        model.addAttribute("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable Long petId, Model model){
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result){
+        if(result.hasErrors())
+            return "pets/createOrUpdateVisitForm";
+        visitService.save(visit);
+        return "redirect:/owners/{ownerId}";
+    }
+}
