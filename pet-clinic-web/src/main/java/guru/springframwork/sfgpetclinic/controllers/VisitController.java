e2271cd8c22580be383351a2a8c87b8839580d06
Ioan Simiciuc <nioasca@gmail.com>
2021-01-15 21:46:01 +0200

    Added new test for visit controller

--- pet-clinic-web/src/main/java/guru/springframwork/sfgpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/guru/springframwork/sfgpetclinic/controllers/VisitController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 
 @Controller
 public class VisitController {
@@ -25,8 +27,15 @@ public class VisitController {
     }
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder){
+    public void dataBinder(WebDataBinder dataBinder){
         dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     @ModelAttribute("visit")
