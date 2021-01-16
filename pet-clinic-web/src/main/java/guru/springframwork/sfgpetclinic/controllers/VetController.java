09873c22960915c376022886d03003cd093cce4a
Ioan Simiciuc <nioasca@gmail.com>
2021-01-16 14:42:13 +0200

    Added vets list view json endpoint. closes #69

--- pet-clinic-web/src/main/java/guru/springframwork/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframwork/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package guru.springframwork.sfgpetclinic.controllers;
 
+import guru.springframwork.sfgpetclinic.model.Vet;
 import guru.springframwork.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +23,10 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
